5288421f7e833cf7de73c7ace67162550ad358b1
sahil777-rgb <vmkbm7gfnf@privaterelay.appleid.com>
2026-09-19 12:04:16 +0530

    Create problem03.c

---
+++ Chapter 3 practice set/problem03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int year;
+    printf("enter year: ");
+    scanf("%d",&year);
+    if(year%4==0 || year%100!=0 || year%400==0){
+        printf("the year is a leap year");
+    }
+    else {
+        printf("this year is not a leap year");
+    }
+    return 0;
+}
